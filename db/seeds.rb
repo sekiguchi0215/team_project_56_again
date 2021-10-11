1302f2cdf9d4e6b2ba3ad99c6f35862eba79c065
sekiguchi0215 <dsphere5296@gmail.com>
2021-10-11 23:09:49 +0900

    seed に、テキスト教材・動画教材のデータを投入する処理を記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "import_csv"
 
 email = "test@example.com"
 password = "password"
@@ -13,3 +14,10 @@ User.find_or_create_by!(email: email) do |user|
   user.password = password
   puts "ユーザーの初期データインポートに成功しました。"
 end
+
+%w[texts movies].each do |table_name|
+  ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name} RESTART IDENTITY CASCADE")
+end
+
+ImportCsv.text_data
+ImportCsv.movie_data
