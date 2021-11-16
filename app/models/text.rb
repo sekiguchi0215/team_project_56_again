b57ef32525f4b72fa449a8b2084f224e41646cbe
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-11-17 07:05:23 +0900

    読破済み機能におけるN+1問題を解消する記述に修正

--- app/models/text.rb
+++ app/models/text.rb
@@ -19,6 +19,6 @@ class Text < ApplicationRecord
   RAILS_GENRE_LIST = %w[basic git ruby rails].freeze
 
   def read_by?(user)
-    read_progresses.exists?(user_id: user.id)
+    read_progresses.any? { |read_progress| read_progress.user_id == user.id }
   end
 end
