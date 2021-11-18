bf81ac325aae257730c2d8ee941f64ba95a098a0
Asuka-I <1400cm@gmail.com>
2021-11-18 18:38:10 +0900

    application_helper.rb にメソッドを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,12 @@ module ApplicationHelper
       "Ruby/Rails動画"
     end
   end
+
+  def text_title
+    if params[:genre] == "php"
+      "PHP テキスト教材"
+    else
+      "Ruby/Railsテキスト教材"
+    end
+  end
 end
