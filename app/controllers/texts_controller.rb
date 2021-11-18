1d3736c3deb7fe889261f15a1ef34af1c1e05258
Asuka-I <1400cm@gmail.com>
2021-11-18 18:57:28 +0900

    texts_controller.rb の index に追記

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,13 @@
 class TextsController < ApplicationController
+  PER_PAGE = 12
+  
   def index
     @texts = Text.where(genre: Text::RAILS_GENRE_LIST)
+    @texts = if params[:genre] == "php"
+               Text.where(genre: Text::PHP_GENRE_LIST).page(params[:page]).per(PER_PAGE)
+             else
+               Text.where(genre: Text::RAILS_GENRE_LIST).page(params[:page]).per(PER_PAGE)
+             end
   end
 
   def show
